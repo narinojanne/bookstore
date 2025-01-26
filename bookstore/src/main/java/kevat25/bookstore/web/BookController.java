181bdce0f525fdc9953be67518ae859563e5e4b0
janne <j_n_812@hotmail.com>
2025-01-26 18:38:05 +0200

    bookcontroller updated

--- bookstore/src/main/java/kevat25/bookstore/web/BookController.java
+++ bookstore/src/main/java/kevat25/bookstore/web/BookController.java
@@ -3,7 +3,6 @@ package kevat25.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
