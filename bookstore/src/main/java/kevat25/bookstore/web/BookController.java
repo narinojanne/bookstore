a1f8f568a4781a16813feb9ec8c8a593202b6212
janne <j_n_812@hotmail.com>
2025-02-12 14:28:05 +0200

    removed unused imports

--- bookstore/src/main/java/kevat25/bookstore/web/BookController.java
+++ bookstore/src/main/java/kevat25/bookstore/web/BookController.java
@@ -1,7 +1,5 @@
 package kevat25.bookstore.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
