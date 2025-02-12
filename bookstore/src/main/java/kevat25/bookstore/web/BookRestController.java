ee8a6a5d907304ef92474e722a39fdabc6a225be
janne <j_n_812@hotmail.com>
2025-02-12 19:08:37 +0200

    added REST edit book by id

--- bookstore/src/main/java/kevat25/bookstore/web/BookRestController.java
+++ bookstore/src/main/java/kevat25/bookstore/web/BookRestController.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kevat25.bookstore.domain.Book;
@@ -25,26 +25,35 @@ public class BookRestController {
 
     // RESTful service to get all books
     @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public @ResponseBody List<Book> bookListRest() {
+    public List<Book> bookListRest() {
         return (List<Book>) bookRepository.findAll();
     }
 
     // RESTful service to get book by id
     @RequestMapping(value = "/books/{id}", method = RequestMethod.GET)
-    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+    public Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
         return bookRepository.findById(bookId);
     }
 
     // RESTful service to save new book
     @RequestMapping(value = "/books", method = RequestMethod.POST)
-    Book newBook(@RequestBody Book newBook) {
-        return bookRepository.save(newBook);
+    public List<Book> newBook(@RequestBody Book newBook) {
+        bookRepository.save(newBook);
+        return (List<Book>) bookRepository.findAll();
     }
 
     // RESTful service to delete book by id
     @RequestMapping(value = "/books/{id}", method = RequestMethod.DELETE)
-    public @ResponseBody void deleteBookRest(@PathVariable("id") Long bookId) {
+    public List<Book> deleteBookRest(@PathVariable("id") Long bookId) {
         bookRepository.deleteById(bookId);
+        return (List<Book>) bookRepository.findAll();
+    }
+
+    // RESTful service to edit book
+    @RequestMapping(value = "/books/{id}", method = RequestMethod.PUT)
+    Book editBook(@RequestBody Book editedBook, @PathVariable("id") Long bookId) {
+        editedBook.setId(bookId);
+        return bookRepository.save(editedBook);
     }
 
 }
