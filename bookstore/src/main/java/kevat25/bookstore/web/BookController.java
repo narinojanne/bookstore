59274cf1b6d0e8a5814d6d22c87e0ba53f950eb2
janne <j_n_812@hotmail.com>
2025-02-12 13:44:05 +0200

    dependecy for REST  added

--- bookstore/src/main/java/kevat25/bookstore/web/BookController.java
+++ bookstore/src/main/java/kevat25/bookstore/web/BookController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
